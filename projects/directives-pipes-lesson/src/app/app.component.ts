0514b10ec431849f9a1a8a2475801ce12708e772
Yauheni Babrounik <yauheni.babrounik@gmail.com>
2024-07-19 20:10:16 +0500

    directives and pipes lesson

---
+++ projects/directives-pipes-lesson/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NgXxxComponent } from './ng-xxx/ng-xxx.component';
+import { CompositionComponent } from './directives/composition/composition.component';
+import { CustomNgifComponent } from './directives/structural/custom-ngif/custom-ngif.component';
+import { CustomNgforComponent } from './directives/structural/custom-ngfor/custom-ngfor.component';
+import { ExportAsComponent } from './directives/export-as/export-as.component';
+import { TrackByComponent } from './track-by/track-by.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    NgXxxComponent,
+    CompositionComponent,
+    CustomNgifComponent,
+    CustomNgforComponent,
+    ExportAsComponent,
+    TrackByComponent,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'directives-pipes-lesson';
+}
